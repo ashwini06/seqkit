8f6d1fd60719bd75d298bc8dd8dfc62e3a5c9097
ashwini06 <ashwini06@users.noreply.github.com>
2016-04-28 18:05:56 +0200

    Update preqc.py

--- seqkit/preqc/preqc.py
+++ seqkit/preqc/preqc.py
@@ -2,6 +2,7 @@
 """Methods and functionalites to do a pre QC"""
 import os
 import pdb
+import subprocess
 from seqkit.utils.find_samples import find_samples
 
 def run_qc(project):
@@ -9,3 +10,7 @@ def run_qc(project):
     samples = find_samples(project)
     pdb.set_trace()
     ## do everything after here ##
+    cmd = ['module','load','FastQC/0.11.5']
+	subprocess.check_all(cmd)
+	cmd = ['fastqc', samples, '-o' ]
+	subprocess.check_all(cmd)
