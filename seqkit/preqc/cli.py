94ec6c82dd1c7851082f54d19d596e3f494d9456
senthil10 <senthilkumar.panneerselvam@scilifelab.se>
2016-04-29 15:48:37 +0200

    access to config file

--- seqkit/preqc/cli.py
+++ seqkit/preqc/cli.py
@@ -6,10 +6,10 @@ import subprocess
 from seqkit.preqc import preqc as qc
         
 @click.command()
-@click.option('-p', '--project', type=click.STRING, help='Project to perform preQC')
-@click.option('-o' , '--out_Dir', type=click.STRING, help='Path to output folder')
+@click.option('-p', '--project', required=True, type=click.STRING, help='Project to perform preQC')
+@click.option('-o' , '--out_dir', type=click.STRING, help='Path to output folder')
 @click.pass_context
-def preqc(ctx, project):
+def preqc(ctx, project, out_dir):
 	""" pre QC methods and utilities """
-	qc.run_qc("/proj/b2012025/RAW_DATA/ChIP_histone/lizzy_andersson_ChIP_ESC_wt_57-64")
+	qc.run_qc(project)
 
--- seqkit/preqc/preqc.py
+++ seqkit/preqc/preqc.py
@@ -2,29 +2,33 @@
 """Methods and functionalites to do a pre QC"""
 import os
 import pdb
+from seqkit import CONFIG as conf
 from seqkit.utils.find_samples import find_samples
 
 def run_qc(project):
     """Will run the QC"""
-    samples = find_samples(project)
-	for sam in samples.keys():
-		fq_fls = samples[sam]
-		pdb.set_trace()
-    	## do everything after here ##
-		fl = open(+sam,'_fastqc.sh','w')
-		fl.write(
-		'#!/bin/bash -l\n'
-		'#SBATCH -A b2012025\n'
-		'#SBATCH -J '+sam''_fastqc'\n'
-		'#SBATCH -p node -N 1 \n'	
-		'#SBATCH -t 2:00:00\n'
-		'#SBATCH --mail-type=FAIL\n'
-		'#SBATCH --mail-user=\'ashwini.jeggari@scilifelab.se\'\n\n'
-		'module load bioinfo-tools\n'
-		'module load FastQC/0.11.5\n'
-		#'op_dir=\'\'\n'
-		#'fastqc -o '+outdir' -f fastq '+fq_fls'\n'
-		'fastqc -f fastq '+fq_fls'\n'
-		)
-	fl.close()	
-	subprocess.check_call(['sbatch', +sam,'_fastqc.sh'])
+    root_dir = conf.get('root_dir','')
+    proj_dir = os.path.join(root_dir, project)
+    fastqc_sbatch_template = ('#!/bin/bash -l\n'
+    		                  '#SBATCH -A b2012025\n'
+    		                  '#SBATCH -J {sam}_fastqc\n'
+    		                  '#SBATCH -p node -N 1 \n'	
+    		                  '#SBATCH -t 2:00:00\n'
+    		                  '#SBATCH --mail-type=FAIL\n'
+    		                  '#SBATCH --mail-user=\'ashwini.jeggari@scilifelab.se\'\n\n'
+    		                  'module load bioinfo-tools\n'
+    		                  'module load FastQC/0.11.5\n'
+    		                  'cd {sam_dir}\n'
+    		                  'fastqc -o fastqc -f fastq {fq_files}\n'
+    		                  )
+    samples = find_samples(proj_dir)
+    for sam in samples.keys():
+        fq_fls = samples[sam]
+        sam_dir = os.path.join(proj_dir, sam)
+        src_dir = os.path.join(sam_dir, scripts)
+        if not os.path.exists(src_dir):
+            os.mkdir(src_dir)
+        job_file = os.path.join(src_dir, "{}_fastqc.sh".format(sam))
+        with open(job_file, 'w') as jb_fl:
+            jb_fl.write(fastqc_sbatch_template.format(sam=sam, sam_dir=sam_dir, fq_files=" ".join(fq_fls)))
+#	    subprocess.check_call(['sbatch', +sam,'_fastqc.sh'])
