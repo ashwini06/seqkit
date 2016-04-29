1e4084d2d6bcb2d88dbd0b62f4c51a36408c2905
ashwini06 <ashwini.jeggari@gmail.com>
2016-04-29 10:53:03 +0200

    modifications

--- seqkit/preqc/preqc.py
+++ seqkit/preqc/preqc.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 """Methods and functionalites to do a pre QC"""
 import os
+import pdb
 from seqkit.utils.find_samples import find_samples
 
 def run_qc(project):
     """Will run the QC"""
     samples = find_samples(project)
+    pdb.set_trace()
     ## do everything after here ##
