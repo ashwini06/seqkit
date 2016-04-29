db65f3af3c2a1e7c152188a4bf7891d3a5b67787
ashwini06 <ashwini.jeggari@gmail.com>
2016-04-29 12:46:09 +0200

    latest change

--- seqkit/preqc/preqc.py
+++ seqkit/preqc/preqc.py
@@ -7,9 +7,24 @@ from seqkit.utils.find_samples import find_samples
 def run_qc(project):
     """Will run the QC"""
     samples = find_samples(project)
-    pdb.set_trace()
-    ## do everything after here ##
-    cmd = ['module','load','FastQC/0.11.5']
-    subprocess.check_all(cmd)
-    cmd = ['fastqc', samples, '-o' ]
-    subprocess.check_all(cmd)
+	for sam in samples.keys():
+		fq_fls = samples[sam]
+		pdb.set_trace()
+    	## do everything after here ##
+		fl = open(+sam,'_fastqc.sh','w')
+		fl.write(
+		'#!/bin/bash -l\n'
+		'#SBATCH -A b2012025\n'
+		'#SBATCH -J '+sam''_fastqc'\n'
+		'#SBATCH -p node -N 1 \n'	
+		'#SBATCH -t 2:00:00\n'
+		'#SBATCH --mail-type=FAIL\n'
+		'#SBATCH --mail-user=\'ashwini.jeggari@scilifelab.se\'\n\n'
+		'module load bioinfo-tools\n'
+		'module load FastQC/0.11.5\n'
+		#'op_dir=\'\'\n'
+		#'fastqc -o '+outdir' -f fastq '+fq_fls'\n'
+		'fastqc -f fastq '+fq_fls'\n'
+		)
+	fl.close()	
+	subprocess.check_call(['sbatch', +sam,'_fastqc.sh'])
