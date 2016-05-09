776c5236818cf5d8f3d6516e7db740204e85518c
ashwini06 <ashwini.jeggari@gmail.com>
2016-05-09 15:38:08 +0200

    testing bam2bed

--- seqkit/analysis/analysis.py
+++ seqkit/analysis/analysis.py
@@ -12,15 +12,18 @@ def run_align(project,aligner):
 	if aligner == "bwa":
 		align_module = 'module load bwa/0.7.12\n'
 		align_index = '/pica/data/uppnex/igenomes/Mus_musculus/Ensembl/GRCm38/Sequence/BWAIndex/genome.fa'
-    		align_block = ('bwa aln {align_index} ${{run}} > {align_dir}/${{nm}}.sai\n'
-                       		'bwa samse {align_index} {align_dir}/${{nm}}.sai ${{run}}.fastq | samtools view -Sb - > {align_dir}/${{nm}}.bam\n'
-				'rm ${{nm}}.sai\n')
+    		align_block = ('bwa aln {align_index} ${{run}} > {align_dir}/${{nam}}.sai\n'
+                       		'bwa samse {align_index} {align_dir}/${{nam}}.sai ${{run}}.fastq | samtools view -Sb - > {align_dir}/${{nam}}.bam\n'
+				'rm ${{nam}}.sai\n'
+				)
 	elif aligner == "bowtie2":
 		align_module = 'module load bowtie2/2.2.6\n'
 		align_index = "/pica/data/uppnex/igenomes/Mus_musculus/Ensembl/GRCm38/Sequence/Bowtie2Index/genome"
-		align_block =  ('bowtie2 -t -p 8 -k2 --very-sensitive -x {align_index} -q ${{run}} -S {align_dir}/${{nm}}.sam > {align_dir}/${{nm}}_bowtie2.log\n'
-                       'samtools view -bS -o {align_dir}/${{nm}}.bam {align_dir}/${{nm}}.sam\n'
-                       'rm {align_dir}/${{nm}}.sam\n')
+		align_block =  ('bowtie2 -t -p 8 -k2 --very-sensitive -x {align_index} -q ${{run}} -S {align_dir}/${{nam}}.sam > {align_dir}/${{nam}}_bowtie2.log\n'
+                       'samtools view -bS -o {align_dir}/${{nam}}.bam {align_dir}/${{nam}}.sam\n'
+		       'rm {align_dir}/${{nam}}.sam\n'
+			)
+	
 
 	align_template = ('#!/bin/bash -l\n'
     		           '#SBATCH -A b2012025\n'
@@ -36,8 +39,13 @@ def run_align(project,aligner):
 			   'if [[ $(ls {sam}/Rawdata/*gz | wc -l) -gt 0 ]]; then gzip -d {sam}/Rawdata/*gz; fi\n'
 			   'for run in {fq_files};do\n'
 			   'nm=$(basename ${{run}})\n'
-			   'nm=${{nm/.fastq/}}\n'
-			   ''+align_block+''
+			   #'nm=${{nm/.fastq/}}\n'
+			   'nm=${{nm/_*/}}\n' 
+			   'nam={sam}"_"${{nm}}\n'
+			    ''+align_block+''
+			   'samtools sort -m 10000000 {align_dir}/${{nam}}.bam {align_dir}/${{nam}}_sorted'
+                      	   'samtools index {align_dir}/${{nam}}_sorted'
+			   'rm {align_dir}/${{nam}}.bam\n'
 			   'done\n'
                         )
 	samples = find_samples(proj_dir)
@@ -47,10 +55,39 @@ def run_align(project,aligner):
 		src_dir = os.path.join(sam_dir, 'scripts')
         	if not os.path.exists(src_dir):
         		os.mkdir(src_dir)
-		align_dir = os.path.join(sam_dir,aligner)
+		align_dir = os.path.join(sam_dir,"{}_{}".format(aligner,"alignment"))
 		if not os.path.exists(align_dir):
 			os.mkdir(align_dir)
         	job_file = os.path.join(src_dir, "{}_{}.sh".format(sam,aligner))
 		with open(job_file, 'w') as jb_fl:
         		jb_fl.write(align_template.format(sam=sam, sam_dir=sam_dir,align_dir=align_dir,proj_dir=proj_dir,align_index=align_index, fq_files=" ".join(fq_fls)))
 		#subprocess.check_call(['sbatch',job_file])
+
+def run_b2b(project):
+	""" Will run the bam to bed file conversion """
+	root_dir = conf.get('root_dir','')
+        proj_dir = os.path.join(root_dir, project)
+	align_b2b = ('#!/bin/bash -l\n'
+		    'module load BEDTools/2.11.2\n'
+		    #'bamToBed -i {align_dir}/${{nam}}_sorted.bam > {bed_dir}/${{nam}}.bed\n'
+	            #'awk -F\\t -v 'OFS=\t' '{print chr$1,$2,$3,".",$5,$6}' {bed_dir}/${{nam}}.bed | sort -u > {bed_dir}/${{nam}}_uniq.bed\n'
+	 	    #'rm {bed_dir}/${{nam}}.bed\n'
+		    )
+
+	samples = find_samples(proj_dir)
+	for sam in samples.keys():
+		src_dir =  os.path.join(proj_dir, sam, 'scripts')
+		align_dir = os.path.join(proj_dir,sam,"{}".format("*alignment"))
+		bed_dir = os.path.join(proj_dir,sam,'bedfiles')
+		if not os.path.exists(bed_dir):	
+			os.mkdir(bed_dir)
+		pdb.set_trace()
+		for file in os.path.listdir(align_dir):
+			if file.endswith("*.bam"):
+				nam=file 
+                job_file = os.path.join(src_dir, "{}_{}.sh".format(sam,bam2bed))
+		with open(job_file, 'w') as jb_fl:
+                        jb_fl.write(align_b2b.format(sam=sam,align_dir=align_dir,proj_dir=proj_dir,bed_dir=bed_dir))
+
+
+	
--- seqkit/analysis/cli.py
+++ seqkit/analysis/cli.py
@@ -4,11 +4,26 @@
 import click
 from seqkit.analysis import analysis as als
         
-@click.command()
-@click.option('-p','--project',required=True, type=click.STRING,help='Project to perform bowtie alignment')
+@click.group()
+def analysis():
+	""" Analysis methods entry point """
+	pass
+
+## analysis subcommands
+@analysis.command()
+@click.option('-p','--project',required=True, type=click.STRING,help='Project to perform alignment')
 @click.option('-o', '--out_dir', type=click.STRING, help='Path to save outputs')
 @click.option('-a','--aligner', default='bowtie2',type=click.STRING,help='which aligner to use bowtie2/bwa, default is "bowtie2"')
 @click.pass_context
-def analysis(ctx, project, out_dir, aligner):
+def align(ctx, project, out_dir, aligner):
 	""" Commands to run analysis/pipelines """
 	als.run_align(project,aligner)
+
+@analysis.command()
+@click.option('-p','--project',required=True, type=click.STRING,help='project to perform bam2bed conversion')
+@click.option('-o', '--out_dir', type=click.STRING, help='Path to save outputs')
+@click.pass_context
+def bamTobed(ctx, project,out_dir):
+	""" Commands to convert bam files to bed files """
+	als.run_b2b(project)
+
