e12100c5590f6b962d6ec671a3b6d1cddec1b2c2
ashwini06 <ashwini.jeggari@gmail.com>
2016-05-09 11:06:03 +0200

    alignment runs added

--- seqkit/analysis/analysis.py
+++ seqkit/analysis/analysis.py
@@ -5,44 +5,52 @@ import pdb
 from seqkit import CONFIG as conf
 from seqkit.utils.find_samples import find_samples
 
-def run_bowtie(project):
-    """Will run the bowtie-alignment"""
-    root_dir = conf.get('root_dir','')
-    proj_dir = os.path.join(root_dir, project)
-    bowtie2_index='/pica/data/uppnex/igenomes/Mus_musculus/Ensembl/GRCm38/Sequence/Bowtie2Index'
-    bowtie2_template = ('#!/bin/bash -l\n'
+def run_align(project,aligner):
+	"""Will run the preferred-alignment"""
+	root_dir = conf.get('root_dir','')
+	proj_dir = os.path.join(root_dir, project)
+	if aligner == "bwa":
+		align_module = 'module load bwa/0.7.12\n'
+		align_index = '/pica/data/uppnex/igenomes/Mus_musculus/Ensembl/GRCm38/Sequence/BWAIndex/genome.fa'
+    		align_block = ('bwa aln {align_index} ${{run}} > {align_dir}/${{nm}}.sai\n'
+                       		'bwa samse {align_index} {align_dir}/${{nm}}.sai ${{run}}.fastq | samtools view -Sb - > {align_dir}/${{nm}}.bam\n'
+				'rm ${{nm}}.sai\n')
+	elif aligner == "bowtie2":
+		align_module = 'module load bowtie2/2.2.6\n'
+		align_index = "/pica/data/uppnex/igenomes/Mus_musculus/Ensembl/GRCm38/Sequence/Bowtie2Index/genome"
+		align_block =  ('bowtie2 -t -p 8 -k2 --very-sensitive -x {align_index} -q ${{run}} -S {align_dir}/${{nm}}.sam > {align_dir}/${{nm}}_bowtie2.log\n'
+                       'samtools view -bS -o {align_dir}/${{nm}}.bam {align_dir}/${{nm}}.sam\n'
+                       'rm {align_dir}/${{nm}}.sam\n')
+
+	align_template = ('#!/bin/bash -l\n'
     		           '#SBATCH -A b2012025\n'
-    		           '#SBATCH -J {sam}_fastqc\n'
+    		           '#SBATCH -J {sam}_align\n'
     		           '#SBATCH -p core -n 2 \n'	
-    		           '#SBATCH -t 2:00:00\n'
+    		           '#SBATCH -t	10:00:00\n'
     		           '#SBATCH --mail-type=FAIL\n'
     		           '#SBATCH --mail-user=\'ashwini.jeggari@scilifelab.se\'\n\n'
     		           'module load bioinfo-tools\n'
-    		           'module load bowtie2/2.2.6\n'
     		           'module load samtools/1.3\n'
+			   ''+align_module+''
 		 	   'cd '+proj_dir+'\n'
 			   'if [[ $(ls {sam}/Rawdata/*gz | wc -l) -gt 0 ]]; then gzip -d {sam}/Rawdata/*gz; fi\n'
 			   'for run in {fq_files};do\n'
 			   'nm=$(basename ${{run}})\n'
 			   'nm=${{nm/.fastq/}}\n'
-			  # 'if [[ ${{run}} == *.gz ]] ; then gunzip '+proj_dir+'/${{run}} ; fi\n'
-			   'bowtie2 -t -p 8 -k2 --very-sensitive -x '+bowtie2_index+'/genome -q ${{run}} -S {bowtie2_dir}/${{nm}}.sam > {bowtie2_dir}/${{nm}}_bowtie2.log\n'
-			   'samtools view -bS -o {bowtie2_dir}/${{nm}}.bam {bowtie2_dir}/${{nm}}.sam\n'
-			   'rm {bowtie2_dir}/${{nm}}.sam \n'
+			   ''+align_block+''
 			   'done\n'
                         )
-    samples = find_samples(proj_dir)
-    for sam in samples.keys():
-        fq_fls = samples[sam]
-	#pdb.set_trace()
-        sam_dir = os.path.join(proj_dir, sam)
-	src_dir = os.path.join(sam_dir, 'scripts')
-        if not os.path.exists(src_dir):
-        	os.mkdir(src_dir)
-	bowtie2_dir = os.path.join(sam_dir,'bowtie2')
-	if not os.path.exists(bowtie2_dir):
-		os.mkdir(bowtie2_dir)
-        job_file = os.path.join(src_dir, "{}_bowtie2.sh".format(sam))
-	with open(job_file, 'w') as jb_fl:
-        	jb_fl.write(bowtie2_template.format(sam=sam, sam_dir=sam_dir,bowtie2_dir=bowtie2_dir, fq_files=" ".join(fq_fls)))
-	#subprocess.check_call(['sbatch',job_file])
+	samples = find_samples(proj_dir)
+	for sam in samples.keys():
+        	fq_fls = samples[sam]
+        	sam_dir = os.path.join(proj_dir, sam)
+		src_dir = os.path.join(sam_dir, 'scripts')
+        	if not os.path.exists(src_dir):
+        		os.mkdir(src_dir)
+		align_dir = os.path.join(sam_dir,aligner)
+		if not os.path.exists(align_dir):
+			os.mkdir(align_dir)
+        	job_file = os.path.join(src_dir, "{}_{}.sh".format(sam,aligner))
+		with open(job_file, 'w') as jb_fl:
+        		jb_fl.write(align_template.format(sam=sam, sam_dir=sam_dir,align_dir=align_dir,proj_dir=proj_dir,align_index=align_index, fq_files=" ".join(fq_fls)))
+		#subprocess.check_call(['sbatch',job_file])
--- seqkit/analysis/cli.py
+++ seqkit/analysis/cli.py
@@ -7,7 +7,8 @@ from seqkit.analysis import analysis as als
 @click.command()
 @click.option('-p','--project',required=True, type=click.STRING,help='Project to perform bowtie alignment')
 @click.option('-o', '--out_dir', type=click.STRING, help='Path to save outputs')
+@click.option('-a','--aligner', default='bowtie2',type=click.STRING,help='which aligner to use bowtie2/bwa, default is "bowtie2"')
 @click.pass_context
-def analysis(ctx, project, out_dir):
+def analysis(ctx, project, out_dir, aligner):
 	""" Commands to run analysis/pipelines """
-	als.run_bowtie(project)
+	als.run_align(project,aligner)
