64b1a0876b3e2af915de37c10ce8c3472f25617c
ashwini06 <ashwini.jeggari@gmail.com>
2016-05-19 15:07:33 +0200

    Arranging project folder

--- misc/arrange_data.sh
+++ misc/arrange_data.sh
@@ -6,13 +6,15 @@
 
 # dir_structure : proj_folder/sample_folder/Raw_data/*fastq ; 
 
+# $dir should point to the path where all the sample folders are present.
+
 dir=$1
 nw_dir=$2
 
 samples=$(ls --color=never $dir);
 for sam in $samples;do
 nm=${dir}/${sam}
-raw_dir=${nm}'/Rawdata'
+raw_dir=${dir}/${sam}'/Rawdata'
 if [ ! -d  ${raw_dir} ]; then mkdir -p ${raw_dir};fi
 mv -t $raw_dir $nm/*.*
 mv $nm $nw_dir
