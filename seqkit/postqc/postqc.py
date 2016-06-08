9854cbc02c342c8152645ddfad9fd11a3d981d40
ashwini06 <ashwini.jeggari@gmail.com>
2016-06-08 12:52:39 +0200

    postqc method-ngsplot added

--- seqkit/postqc/postqc.py
+++ seqkit/postqc/postqc.py
@@ -21,7 +21,8 @@ def bamcov(project, genefile, input_file):
                        '#SBATCH --mail-type=FAIL\n'
                        '#SBATCH --mail-user=\'ashwini.jeggari@scilifelab.se\'\n\n'
                        'module load bioinfo-tools\n'
-                       'module load deepTools/2.2.3\n\n'
+                       'module load deepTools/2.2.3\n'
+                       'module load ngsplot/2.61\n\n'
                        #'bamCompare --bamfile1 {treatment} --bamfile2 {control} --binSize 25 --ratio log2 --scaleFactorsMethod SES -o {postqc_dir}/{treat}_Vs_{ctrl}_log2ratio.bw\n'
                        'bamCoverage --bam {treatment} --binSize 25 --normalizeUsingRPKM -o {postqc_dir}/{treat}_coverage.bw -bl /home/ashwini/mm10_blacklisted-regions.bed\n'
                        'bamCoverage --bam {control} --binSize 25 --normalizeUsingRPKM -o {postqc_dir}/{ctrl}_coverage.bw -bl /home/ashwini/mm10_blacklisted-regions.bed\n'
@@ -29,7 +30,9 @@ def bamcov(project, genefile, input_file):
                        'multiBamSummary bins --bamfiles '+proj_dir+'/*/alignment_bowtie/bam_files/*_rmdup.bam -out {postqc_dir}/results.npz\n'
                        'plotCorrelation --corData {postqc_dir}/results.npz --plotFile {postqc_dir}/scatterplot.pdf --corMethod pearson --whatToPlot scatterplot --skipZeros\n'
                        'computeMatrix scale-regions -S {postqc_dir}/{treat}_coverage.bw {postqc_dir}/{ctrl}_coverage.bw -R {ucsc_file} --skipZeros -o {postqc_dir}/matrix.mat.gz --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000\n'
-                       'plotHeatmap -m {postqc_dir}/matrix.mat.gz -out {postqc_dir}/{treat}_Vs_{ctrl}_heatmap.png\n')
+                       'plotHeatmap -m {postqc_dir}/matrix.mat.gz -out {postqc_dir}/{treat}_Vs_{ctrl}_heatmap.png\n'
+                       'ngs.plot.r -G mm10 -R genebody -C {treatment}:{control} -O {treat}_Vs_{ctrl}.genebody -T {treat}\n'
+                       'ngs.plot.r -G mm10 -R tss -C {treatment}:{control} -O {treat}_Vs_{ctrl}.tss -T {treat} -L 3000 -FL 3000\n')
 
 
     ucsc_file = genefile
